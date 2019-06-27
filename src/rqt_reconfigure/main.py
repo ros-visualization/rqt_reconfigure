cf4daf0cb331998ca401cb027113df81af443a30
Gonzalo de Pedro <gonzalo@depedro.com.ar>
2019-06-27 16:23:06 -0300

    Removed comment
    Signed-off-by: Gonzalo de Pedro <gonzalo@depedro.com.ar>

--- src/rqt_reconfigure/main.py
+++ src/rqt_reconfigure/main.py
@@ -6,7 +6,6 @@ from rqt_gui.main import Main
 
 def main():
     plugin = 'rqt_reconfigure.param_plugin.ParamPlugin'
-#    main = Main(filename=plugin)
     sys.exit(Main().main(sys.argv, standalone=plugin))
 
 if __name__ == '__main__':
